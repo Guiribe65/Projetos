1ddef48a88513a3b895b6d87a1c1eb9e8113eaed
Guilherme Costa <guilherme.costa@panmetal.local>
2023-12-13 11:35:50 -0300

    Falta aprender colocar icones

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -3,8 +3,14 @@ import "./Body.css";
 
 function Body() {
   return (
-    <div className="mnu">
-     Teste
+    <div class="mnu"> 
+      <div class="promo">Promoções</div>
+      <div class="massas">Massas</div>
+      <div class="bebidas">Bebidas</div>
+      <div class="sobremesas">Sobremesas</div>
+      <div class="marmitas">Marmitas</div>
+      <div class="msp">Monte seu Prato</div>
+      <div class="contato">Contato</div>
     </div>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Menu from "../menu/Menu";
+// import Logo from "../Imagens/Alvorada.jpg";
 import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
       <header>
+        {/* <img src={Logo} alt="Alvorada Logo" />     */}
         <Menu>Alvorada</Menu>
         <div class="login">Login</div>
         <div class="subtxt">Carnes & Massas</div>
