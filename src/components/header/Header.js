56d031a1bdbb0757cafb11279e4e53fb30d9e8f5
Guilherme Costa <guilherme.costa@panmetal.local>
2023-12-15 13:49:56 -0300

    Adicionar imagens

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,17 +1,30 @@
 import React from "react";
-import Menu from "../menu/Menu";
+import { FaWhatsapp } from "react-icons/fa";
+import { IoPersonCircleOutline, IoSearch} from "react-icons/io5";
+import { MdOutlineShoppingBasket } from "react-icons/md";
 // import Logo from "../Imagens/Alvorada.jpg";
 import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
-      <header>
-        {/* <img src={Logo} alt="Alvorada Logo" />     */}
-        <Menu>Alvorada</Menu>
-        <div class="login">Login</div>
-        <div class="subtxt">Carnes & Massas</div>
-      </header>
+      {/* <img src={Logo} alt="Alvorada Logo" />     */}
+      <MdOutlineShoppingBasket className="shop" />
+      <IoSearch className="search"/>
+      <FaWhatsapp className="WA" />
+      <IoPersonCircleOutline className="login"/>
+      <h1 className="menu">Alvorada</h1>
+      <div class="subtxt">Carnes & Massas</div>
+       {/* -----------------------Menu---------------------------  */}
+      <div class="mnu"> 
+        <div class="promo">Promoções</div>
+        <div class="massas">Massas</div>
+        <div class="bebidas">Bebidas</div>
+        <div class="sobremesas">Sobremesas</div>
+        <div class="marmitas">Marmitas</div>
+        <div class="msp">Monte seu Prato</div>
+        <div class="contato">Contato</div>
+      </div>
     </div>
   );
 }
