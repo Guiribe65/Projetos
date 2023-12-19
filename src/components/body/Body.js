dd4e4491035008cb8d3e82f790d8866ba7c7a4ba
Guilherme Costa <guilherme.costa@panmetal.local>
2023-12-19 13:29:49 -0300

    19/12

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,50 +1,87 @@
 import React from "react";
 import "./Body.css";
-import MinhaImagem from './Imagens/Alvorada.jpg';
+import Alvorada from './Imagens/Alvorada.jpg';
 
 function Body() {
   return (
     <div class="corpo">
-        <h2 class="TP">Promoções</h2>
+        <h2 class="TP" id="Promocoes">Promoções</h2>
         <div class="promo1">
-          <img src={MinhaImagem} alt="Descrição da imagem" />
+          <img src={Alvorada} alt="Descrição da imagem" />
           <div class="ptxt">
             <div class="titulo">Alvorad carnes e massas</div>
-            <div class="descricao">Avnida aclimação jd aquarios saojose dos campos</div>
+            <div class="descricao">Avenida aclimação jd aquarios sao jose dos campos</div>
+            <div class="preco">A partir de $20,00</div>
           </div>
         </div>
         <div class="promo2">
-          <img src={MinhaImagem} alt="Descrição da imagem" />
+          <img src={Alvorada} alt="Descrição da imagem" />
           <div class="ptxt">
             <div class="titulo">Alvorad carnes e massas</div>
             <div class="descricao">Avnida aclimação jd aquarios saojose dos campos</div>
           </div>
         </div>
         <div class="promo3">
-          <img src={MinhaImagem} alt="Descrição da imagem" />
+          <img src={Alvorada} alt="Descrição da imagem" />
           <div class="ptxt">
             <div class="titulo">Alvorad carnes e massas</div>
             <div class="descricao">Avnida aclimação jd aquarios saojose dos campos</div>
           </div>
         </div>
         <div class="promo4">
-          <img src={MinhaImagem} alt="Descrição da imagem" />
+          <img src={Alvorada} alt="Descrição da imagem" />
           <div class="ptxt">
             <div class="titulo">Alvorad carnes e massas</div>
             <div class="descricao">Avnida aclimação jd aquarios saojose dos campos</div>
           </div>
         </div>
         {/* -----------------------Massas---------------------------  */}
-        <h2 class="TM">Massas</h2>
+        <h2 class="TM" id="Massas">Massas</h2>
         <div class="massa1">
-          <img src={MinhaImagem} alt="Descrição da imagem" />
+          <img src={Alvorada} alt="Descrição da imagem" />
           <div class="ptxt">
             <div class="titulo">Raviole de mussarela</div>
             <div class="descricao">Raviole de mussarela de bufula com tomate seco</div>
           </div>
         </div>
+        <div class="massa2">
+          <img src={Alvorada} alt="Descrição da imagem" />
+          <div class="ptxt">
+            <div class="titulo">Raviole de mussarela</div>
+            <div class="descricao">Raviole de mussarela de bufula com tomate seco</div>
+          </div>
+        </div>
+        {/* -----------------------Marmitas---------------------------  */}
+        <h2 class="TMTX" id="Marmitas">Marmitas</h2>
+        <div class="marmitaM">
+          <img src={Alvorada} alt="Descrição da imagem" />
+          <div class="ptxt">
+            <div class="titulo">Marmitex Mini</div>
+            <div class="descricao">...</div>
+          </div>
+        </div>
+        <div class="marmitaN">
+          <img src={Alvorada} alt="Descrição da imagem" />
+          <div class="ptxt">
+            <div class="titulo">Marmitex Normal</div>
+            <div class="descricao">...</div>
+          </div>
+        </div>
+        <div class="marmitaE">
+          <img src={Alvorada} alt="Descrição da imagem" />
+          <div class="ptxt">
+            <div class="titulo">Marmitex Executiva</div>
+            <div class="descricao">...</div>
+          </div>
+        </div>
+        <div class="marmitaMist">
+          <img src={Alvorada} alt="Descrição da imagem" />
+          <div class="ptxt">
+            <div class="titulo">Marmitex Mistura</div>
+            <div class="descricao">...</div>
+          </div>
+        </div>
     </div>
-
   );
 }
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,25 +1,88 @@
-import React from "react";
+import React,  { useState } from "react";
 import { FaWhatsapp } from "react-icons/fa";
 import { IoPersonCircleOutline, IoSearch} from "react-icons/io5";
 import { MdOutlineShoppingBasket } from "react-icons/md";
+import { IoMdClose } from "react-icons/io";
 import "./Header.css";
 
 function Header() {
+
+  const [mostrarCampoPesquisa, setMostrarCampoPesquisa] = useState(false);
+
+  const toggleCampoPesquisa = () => {
+    setMostrarCampoPesquisa(!mostrarCampoPesquisa);
+  };
+
+   const rolarParaPromocoes = () => {
+    const Promocoes = document.getElementById("Promocoes");
+    const alturaDoHeader = document.querySelector(".header").offsetHeight;
+
+    if (Promocoes) {
+      const posicaoTopo = Promocoes.offsetTop - alturaDoHeader;
+
+      window.scrollTo({
+        top: posicaoTopo,
+        behavior: "smooth",
+      });
+    }
+  };
+
+    const rolarParaMassas = () => {
+      const Massas = document.getElementById("Massas");
+      const alturaDoHeader = document.querySelector(".header").offsetHeight;
+      const espacoExtra = 40; 
+    
+      if (Massas) {
+        const posicaoTopo = Massas.offsetTop - alturaDoHeader + espacoExtra;
+    
+        window.scrollTo({
+          top: posicaoTopo,
+          behavior: "smooth",
+        });
+      }
+    };
+
+    const rolarParaMarmitas = () => {
+      const Marmitas = document.getElementById("Marmitas");
+      const alturaDoHeader = document.querySelector(".header").offsetHeight;
+      const espacoExtra = 40; 
+    
+      if (Marmitas) {
+        const posicaoTopo = Marmitas.offsetTop - alturaDoHeader + espacoExtra;
+    
+        window.scrollTo({
+          top: posicaoTopo,
+          behavior: "smooth",
+        });
+      }
+    };
+
   return (
     <div className="header">
+      {mostrarCampoPesquisa ? (
+        <IoMdClose className="close" onClick={toggleCampoPesquisa} />
+      ) : (
+        <IoSearch className="search" onClick={toggleCampoPesquisa} />
+      )}
       <MdOutlineShoppingBasket className="shop" />
-      <IoSearch className="search"/>
       <FaWhatsapp className="WA" />
       <IoPersonCircleOutline className="login"/>
       <h1 className="menu">Alvorada</h1>
       <div class="subtxt">Carnes & Massas</div>
+
+      {mostrarCampoPesquisa && (
+        <div className="pesquisa">
+          <IoSearch className="icone-pesquisa" />
+          <input type="text" placeholder="Pesquisar..." className="campo-pesquisa"/>
+        </div>
+      )}
        {/* -----------------------Menu---------------------------  */}
       <div class="mnu"> 
-        <div class="promo">Promoções</div>
-        <div class="massas">Massas</div>
+        <div class="promo" onClick={rolarParaPromocoes}>Promoções</div>
+        <div class="massas" onClick={rolarParaMassas}>Massas</div>
         <div class="bebidas">Bebidas</div>
         <div class="sobremesas">Sobremesas</div>
-        <div class="marmitas">Marmitas</div>
+        <div class="marmitas" onClick={rolarParaMarmitas}>Marmitas</div>
         <div class="msp">Monte seu Prato</div>
         <div class="contato">Contato</div>
       </div>
