9044fef5498223edf8db930813b75b80632c87cd
Guilherme Costa <guilherme.costa@panmetal.local>
2024-01-05 12:36:13 -0300

    90% front principal

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useState, useEffect  } from "react";
 import "./Body.css";
 import Alvorada from './Imagens/Alvorada.jpg';
 
 function Body() {
+   // Estado para controlar a visibilidade da nova div
+   const [selecaoMassa1, setSelecaoMassa1] = useState(false);
+   const [selecaoMassa2, setSelecaoMassa2] = useState(false);
+   const [overlayVisivel, setOverlayVisivel] = useState(false);
+
+   // Efeito para adicionar/remover a classe ao body
+  useEffect(() => {
+    if (overlayVisivel) {
+      document.body.classList.add("noscroll");
+    } else {
+      document.body.classList.remove("noscroll");
+    }
+  }, [overlayVisivel]);
+
+
+   // Função para manipular o clique e mostrar a nova div
+   const Massa1 = () => {
+     setSelecaoMassa1(!selecaoMassa1);
+     setSelecaoMassa2(false);
+     setOverlayVisivel(true);
+   };
+
+   const Massa2 = () => {
+    setSelecaoMassa2(!selecaoMassa2);
+    setSelecaoMassa1(false);
+    setOverlayVisivel(true);
+  };
+
+  // Função para fechar a nova div e o overlay
+  const fecharDiv = () => {
+    setSelecaoMassa1(false);
+    setSelecaoMassa2(false);
+    setOverlayVisivel(false);
+  };
+
   return (
     <div class="corpo">
         <h2 class="TP" id="Promocoes">Promoções</h2>
@@ -37,20 +72,24 @@ function Body() {
         </div>
         {/* -----------------------Massas---------------------------  */}
         <h2 class="TM" id="Massas">Massas</h2>
-        <div class="massa1">
+        <div class="massa1"  onClick={Massa1}>
           <img src={Alvorada} alt="Descrição da imagem" />
           <div class="ptxt">
             <div class="titulo">Raviole de mussarela</div>
             <div class="descricao">Raviole de mussarela de bufula com tomate seco</div>
           </div>
         </div>
-        <div class="massa2">
+        {selecaoMassa1 && <div className={`selmassa ${selecaoMassa1 ? 'aparecer' : ''}`}>
+        Nova Div1
+        </div>}
+        <div class="massa2" onClick={Massa2}>
           <img src={Alvorada} alt="Descrição da imagem" />
           <div class="ptxt">
             <div class="titulo">Raviole de mussarela</div>
             <div class="descricao">Raviole de mussarela de bufula com tomate seco</div>
           </div>
         </div>
+        {selecaoMassa2 && <div class="selmassa">Nova Div2</div>}
         {/* -----------------------Marmitas---------------------------  */}
         <h2 class="TMTX" id="Marmitas">Marmitas</h2>
         <div class="marmitaM">
@@ -81,6 +120,65 @@ function Body() {
             <div class="descricao">...</div>
           </div>
         </div>
+        {/* -----------------------MSP---------------------------  */}
+        <h2 class="TMSP" id="msp">Monte seu Prato</h2>
+        <div class="msp1">
+          <img src={Alvorada} alt="Descrição da imagem" />
+          <div class="ptxt">
+            <div class="titulo">Monte seu Talharim</div>
+            <div class="descricao">200g de Talharim +1 Molho.</div>
+          </div>
+        </div>
+        <div class="msp2">
+          <img src={Alvorada} alt="Descrição da imagem" />
+          <div class="ptxt">
+            <div class="titulo">Monte seu Raviole</div>
+            <div class="descricao">200g de Raviole recheado a sua escolha +1 Molho.</div> 
+          </div>
+        </div>
+         {/* -----------------------Sobremesas---------------------------  */}
+        <h2 class="TS" id="sobremesa">Sobremesas</h2>
+        <div class="sobremesa1">
+          <img src={Alvorada} alt="Descrição da imagem" />
+          <div class="ptxt">
+            <div class="titulo">Pudim</div>
+            <div class="descricao">pudim de leite condensado.</div>
+          </div>
+        </div>
+        <div class="sobremesa2">
+          <img src={Alvorada} alt="Descrição da imagem" />
+          <div class="ptxt">
+            <div class="titulo">Torta Holandesa</div>
+            <div class="descricao">Torta holandesa direto da Holanda.</div>
+          </div>
+        </div>
+         {/* -----------------------Bebidas---------------------------  */}
+        <h2 class="TB" id="bebidas">Bebidas</h2>
+        <div class="bebida1">
+          <img src={Alvorada} alt="Descrição da imagem" />
+          <div class="ptxt">
+            <div class="titulo">Refrigerante Lata</div>
+            <div class="descricao">Coca-Cola, Fanta Guarana, Sprite e outros</div>
+            <div class="preco">A partir de $5,00</div>
+          </div>
+        </div>
+        <div class="bebida2">
+          <img src={Alvorada} alt="Descrição da imagem" />
+          <div class="ptxt">
+            <div class="titulo">Refrigerante 2L</div>
+            <div class="descricao">Coca-Cola, Fanta Guarana, Sprite e outros</div>
+            <div class="preco">A partir de $10,00</div>
+          </div>
+        </div>
+        <div class="bebida3">
+          <img src={Alvorada} alt="Descrição da imagem" />
+          <div class="ptxt">
+            <div class="titulo">Água Mineral</div>
+            <div class="descricao">Água com gás ou sem gás</div>
+            <div class="preco">A partir de $3,00</div>
+          </div>
+        </div>
+        {overlayVisivel && <div className="overlay" onClick={fecharDiv}></div>}
     </div>
   );
 }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,17 +1,20 @@
-import React,  { useState } from "react";
+import React,  { useState,  useEffect } from "react";
 import { FaWhatsapp } from "react-icons/fa";
 import { IoPersonCircleOutline, IoSearch} from "react-icons/io5";
 import { MdOutlineShoppingBasket } from "react-icons/md";
 import { IoMdClose } from "react-icons/io";
 import "./Header.css";
+import Body from "../body/Body";
 
 function Header() {
 
   const [mostrarCampoPesquisa, setMostrarCampoPesquisa] = useState(false);
+  const [overlayVisivel, setOverlayVisivel] = useState(false);
 
   const toggleCampoPesquisa = () => {
     setMostrarCampoPesquisa(!mostrarCampoPesquisa);
   };
+
 
    const rolarParaPromocoes = () => {
     const Promocoes = document.getElementById("Promocoes");
@@ -57,6 +60,51 @@ function Header() {
       }
     };
 
+    const rolarParaMonte = () => {
+      const Monte = document.getElementById("msp");
+      const alturaDoHeader = document.querySelector(".header").offsetHeight;
+      const espacoExtra = 40; 
+    
+      if (Monte) {
+        const posicaoTopo = Monte.offsetTop - alturaDoHeader + espacoExtra;
+    
+        window.scrollTo({
+          top: posicaoTopo,
+          behavior: "smooth",
+        });
+      }
+    };
+
+    const rolarParaSobremesa = () => {
+      const Sobremesa = document.getElementById("sobremesa");
+      const alturaDoHeader = document.querySelector(".header").offsetHeight;
+      const espacoExtra = 40; 
+    
+      if (Sobremesa) {
+        const posicaoTopo = Sobremesa.offsetTop - alturaDoHeader + espacoExtra;
+    
+        window.scrollTo({
+          top: posicaoTopo,
+          behavior: "smooth",
+        });
+      }
+    };
+
+    const rolarParaBebidas = () => {
+      const Bebidas = document.getElementById("bebidas");
+      const alturaDoHeader = document.querySelector(".header").offsetHeight;
+      const espacoExtra = 40; 
+    
+      if (Bebidas) {
+        const posicaoTopo = Bebidas.offsetTop - alturaDoHeader + espacoExtra;
+    
+        window.scrollTo({
+          top: posicaoTopo,
+          behavior: "smooth",
+        });
+      }
+    };
+
   return (
     <div className="header">
       {mostrarCampoPesquisa ? (
@@ -80,12 +128,13 @@ function Header() {
       <div class="mnu"> 
         <div class="promo" onClick={rolarParaPromocoes}>Promoções</div>
         <div class="massas" onClick={rolarParaMassas}>Massas</div>
-        <div class="bebidas">Bebidas</div>
-        <div class="sobremesas">Sobremesas</div>
+        <div class="bebidas" onClick={rolarParaBebidas}>Bebidas</div>
+        <div class="sobremesas" onClick={rolarParaSobremesa}>Sobremesas</div>
         <div class="marmitas" onClick={rolarParaMarmitas}>Marmitas</div>
-        <div class="msp">Monte seu Prato</div>
+        <div class="msp" onClick={rolarParaMonte}>Monte seu Prato</div>
         <div class="contato">Contato</div>
       </div>
+      {overlayVisivel && <Body overlayVisivel={overlayVisivel} setOverlayVisivel={setOverlayVisivel} />}
     </div>
   );
 }
